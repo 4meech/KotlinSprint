293f96b2c49191c7afda908024e9db03b0b34847
4meech <hornax@gmail.com>
2024-04-23 23:01:46 +0300

    KS-2-5 compound interest rate calculation

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+    val initialDeposit = 70_000
+    val interestRate = 16.7
+    val depositTerm = 20
+
+    val depositFinal = (initialDeposit * (1 + (interestRate / 100)).pow(depositTerm))
+
+    println("Начальный размер вклада: ${initialDeposit} руб.")
+    println("Годовая процентная ставка: ${interestRate}%")
+    println("Срок вклада, полных лет: ${depositTerm}")
+    //  если не обращать внимания на формат десятичного разделителя, то вывод соответствует условию
+    println("К концу срока вклада сумма будет равна: " + "%.3f".format(depositFinal) + " руб.")
+}
