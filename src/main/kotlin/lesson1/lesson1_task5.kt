a29330ca2535d6f6743a093e5752e67dea6a423d
4meech <hornax@gmail.com>
2024-04-23 11:00:20 +0300

    KS-1-5.2 got rid of magic numbers, fixed an output string in line 13

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,11 +1,15 @@
 package org.example.lesson1
 
 fun main() {
+//  объявлены константы времени
     val totalSeconds = 6480
+    val SECONDS_IN_HOUR = 3600
+    val SECONDS_IN_MINUTE = 60
 
-    val hours = totalSeconds / 3600
-    val minutes = (totalSeconds % 3600) / 60
-    val seconds = "%02d".format(totalSeconds % 60)
+    val hoursInSpace = totalSeconds / SECONDS_IN_HOUR
+    val minutesInSpace = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val secondsInSpace = totalSeconds % SECONDS_IN_MINUTE
 
-    println("Время, проведенное в космосе: ${hours}:${minutes}:${seconds}")
+//  применяем интерполяцию сразу ко всей строке вывода
+    println("%02d:%02d:%02d".format(hoursInSpace, minutesInSpace, secondsInSpace))
 }
