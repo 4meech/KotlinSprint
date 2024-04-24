8df41e6288b6b756c553731044bfea266f59791c
4meech <hornax@gmail.com>
2024-04-24 10:36:18 +0300

    KS-2-5 constant values fixed

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,10 +1,10 @@
 package org.example.lesson1
 
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
+
 fun main() {
-//  объявлены константы времени
     val totalSeconds = 6480
-    val SECONDS_IN_HOUR = 3600
-    val SECONDS_IN_MINUTE = 60
 
     val hoursInSpace = totalSeconds / SECONDS_IN_HOUR
     val minutesInSpace = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
