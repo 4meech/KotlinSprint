2b5baccf4d8a67d91004a6c4a2f3d5d6fb9ed6e3
4meech <hornax@gmail.com>
2024-04-24 11:58:09 +0300

    KS-2-4 initial buff value initialized as a constant, difference between "Percent" and "Percentage" is more clear now; changed the way of counting percentage – multiplication by 0.01 instead of dividing by 100.

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,9 +1,11 @@
 package org.example.lesson_2
 
+const val BUFF_PERCENT = 20
+
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buffPercentage = 0.20
+    val buffPercentage = BUFF_PERCENT * 0.01
 
     val bonusCrystals = (crystalOre * buffPercentage).toInt()
     val bonusIron = (ironOre * buffPercentage).toInt()
