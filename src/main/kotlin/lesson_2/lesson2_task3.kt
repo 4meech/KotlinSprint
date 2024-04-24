64877df2b8b958bec1ba8b6e225c035c92e760c1
4meech <hornax@gmail.com>
2024-04-24 11:15:38 +0300

    KS-2-3 got rid of magic number, constant time values added

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,13 +1,19 @@
 package org.example.lesson_2
 
+const val MINUTES_IN_HOUR = 60
+//  Считаем, что количество часов в сутках строго равно 24 :)
+const val HOURS_IN_DAY = 24
+//  Если допускается рассчитывать значение константы таким образом, то оставим так
+const val MINUTES_IN_DAY = MINUTES_IN_HOUR * HOURS_IN_DAY
+
 fun main() {
     val departureHour = 9
     val departureMinute = 39
     val minutesEnRoute = 457
-//    Приводим всё к минутам и проверяем на прибытие в следующий календарный день (60 * 24)
-    val minutesTotal = (departureHour * 60 + departureMinute + minutesEnRoute) % 1440
-    val arrivalHour = minutesTotal / 60
-    val arrivalMinute = minutesTotal % 60
+//    Приводим всё к минутам и проверяем на прибытие в следующий календарный день (делим на количество минут в дне)
+    val minutesTotal = (departureHour * MINUTES_IN_HOUR + departureMinute + minutesEnRoute) % MINUTES_IN_DAY
+    val arrivalHour = minutesTotal / MINUTES_IN_HOUR
+    val arrivalMinute = minutesTotal % MINUTES_IN_HOUR
 //    Форматируем вывод для корректного отображения значений час/мин с ведущим нулём
     println("Время прибытия поезда: " + "%02d:%02d".format(arrivalHour, arrivalMinute))
 }
