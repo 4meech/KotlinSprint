1ac8434d16bfaf34c477d5de988ed6fb314e24f9
4meech <hornax@gmail.com>
2024-04-24 10:44:16 +0300

    Revert "KS-2-5 name fix"
    This reverts commit e990509195fa387c78915ee984eef599c73d9e6c.

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,7 +1,7 @@
 package org.example.lesson_2
 
 fun main() {
-    val studentsTotal = 4
+    val STUDENTS_TOTAL = 4
 
     val score1 = 3
     val score2 = 4
@@ -9,7 +9,7 @@ fun main() {
     val score4 = 5
 
 //  в рамках задачи не хочется целочисленные переменные явно приводить к дробным
-    val averageScore = (score1 + score2 + score3 + score4).toDouble() / studentsTotal
+    val averageScore = (score1 + score2 + score3 + score4).toDouble() / STUDENTS_TOTAL
 
     println("Средний былл класса по английскому языку: ${averageScore}")
 }
