91633d1c2325508e867361e712a05f62992b6fb7
4meech <hornax@gmail.com>
2024-04-27 12:23:45 +0300

    KS-5-3 comment line removed

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -5,7 +5,7 @@ fun main() {
     val tentIsOpen = true
     val humidity = 20
     val season = "Зима"
-//  проверяем благоприятные ли условия
+
     val isFavorable = (isSunny && tentIsOpen && humidity == 20 && !season.equals("зима", ignoreCase = true))
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorable")
 }
