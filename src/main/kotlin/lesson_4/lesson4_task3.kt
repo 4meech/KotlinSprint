d99d45c89b63b2c87f51798aca2cc3962add4a4c
4meech <hornax@gmail.com>
2024-04-28 13:19:11 +0300

    KS-4-3-3 style edit

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -6,7 +6,7 @@ const val HUMIDITY_PERCENT = 20
 const val SEASON = "Зима"
 
 fun main() {
-    val isFavorable = (IS_SUNNY && IS_AWNING_OPEN && HUMIDITY_PERCENT == 20
-            && !SEASON.equals("зима", ignoreCase = true))
+    val isFavorable = (IS_SUNNY && IS_AWNING_OPEN && HUMIDITY_PERCENT == 20 &&
+            !SEASON.equals("зима", ignoreCase = true))
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorable")
 }
