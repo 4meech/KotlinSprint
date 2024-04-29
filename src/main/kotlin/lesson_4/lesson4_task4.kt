0cdf1b8a180a57376436aaad706fa6666a1ea6d8
4meech <hornax@gmail.com>
2024-04-29 13:41:46 +0300

    KS-4-4 code shortened, redundant vars removed

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,25 +1,13 @@
 package org.example.lesson_4
 
 fun main() {
-    var (isHands, isAbs) = false to false
-    var (isLegs, isBack) = false to false
     val currentDay = 5
-
-    when (currentDay % 2) {
-        0 -> {
-            isLegs = true
-            isBack = true
-        }
-        1 -> {
-            isHands = true
-            isAbs = true
-        }
-    }
+    val isEven = (currentDay % 2 == 0)
 
     println("""
-        Упражнения для рук:      $isHands
-        Упражнения для ног:      $isLegs
-        Упражнения для спины:    $isBack
-        Упражнения для пресса:   $isAbs
+        Упражнения для рук:      ${!isEven}
+        Упражнения для ног:      $isEven
+        Упражнения для спины:    $isEven
+        Упражнения для пресса:   ${!isEven}
     """.trimIndent())
 }
