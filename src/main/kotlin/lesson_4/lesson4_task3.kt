6aa2c1b7215cf82274e46b1663b8ff72cafbe497
4meech <hornax@gmail.com>
2024-04-29 17:29:04 +0300

    KS-5-5-3 isOpen awning const renamed

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_4
 
 const val IS_SUNNY = true
-const val IS_AWNING_OPEN = true
+const val IS_AWNING_OPEN_NOW = true
 const val HUMIDITY_PERCENT = 20
 const val SEASON = "Зима"
 
 fun main() {
-    val isFavorable = (IS_SUNNY && IS_AWNING_OPEN && HUMIDITY_PERCENT == 20 &&
+    val isFavorable = (IS_SUNNY && IS_AWNING_OPEN_NOW && HUMIDITY_PERCENT == 20 &&
             !SEASON.equals("зима", ignoreCase = true))
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorable")
 }
