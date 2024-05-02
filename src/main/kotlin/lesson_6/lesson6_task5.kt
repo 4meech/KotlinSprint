f311a387cfdfd1caac928731cb60a1131b59e1eb
4meech <hornax@gmail.com>
2024-05-02 18:01:25 +0300

    KS-6-5-2 redundant code removed, "wrong answer" logic and refactored

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -1,28 +1,12 @@
 package org.example.lesson_6
 
-import kotlin.random.Random
-import kotlin.random.nextInt
-
 fun main() {
-
-    println("Создайте логин")
-    val logUser = readln()
-    println("Придумайте пароль")
-    val passUser = readln()
-    println("Регистрация прошла успешно")
-
-    do {
-        println("Введите логин")
-        val logEnter = readln()
-        println("Введите пароль")
-        val passEnter = readln()
-    } while (logEnter != logUser || passEnter != passUser)
 
     var tryCount = 3
 
     while (tryCount > 0) {
-        val captchaNumA = Random.nextInt(1..9)
-        val captchaNumB = Random.nextInt(1..9)
+        val captchaNumA = (1..9).random()
+        val captchaNumB = (1..9).random()
         val captchaAnswer = captchaNumA + captchaNumB
 
         println(
@@ -31,14 +15,18 @@ fun main() {
             Сколько будет '$captchaNumA + $captchaNumB'?
         """.trimIndent()
         )
+
         val userInput = readln().toInt()
         if (userInput == captchaAnswer) {
             println("Добро пожаловать!")
             return
         } else {
-            println("Неверно. Попробуйте ещё раз!")
+            print("Неверно! ")
+            if (tryCount > 1) {
+                println("Попробуйте ещё раз.")
+            }
             tryCount--
         }
     }
-    println("Доступ запрещён")
+    println("Доступ запрещён!")
 }
