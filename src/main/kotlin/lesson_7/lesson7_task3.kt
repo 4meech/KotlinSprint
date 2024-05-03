3d6be85a14e00c7f2dbcdfe20162f3cbbacbc720
4meech <hornax@gmail.com>
2024-05-03 19:08:02 +0300

    KS-7-3-2 redundant code removed

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -3,12 +3,7 @@ package org.example.lesson_7
 fun main() {
     print("Введите число: ")
     val userInput = readln().toInt()
-    val intRange: IntProgression = (0 .. userInput step 2)
-    var result = ""
-
-    for (i in intRange) {
-        result += i
+    for (i in 0..userInput step 2) {
+        println(i)
     }
-
-    print(result)
 }
