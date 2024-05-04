dc5755b6e44c84b6ab8f0cb1b4c67f3a7a6b56a8
4meech <hornax@gmail.com>
2024-05-04 12:35:57 +0300

    KS-8-2-2 typo fixed, break->return, redundant variable removed

--- src/main/kotlin/lesson_8/lesson8_task2.kt
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -1,20 +1,16 @@
 package org.example.lesson_8
 
 fun main() {
-    val arrayOfIngridients = arrayOf("хлеб", "масло", "колбаса")
+    val arrayOfIngredients = arrayOf("хлеб", "масло", "колбаса")
 
     println("Введите название ингредиента:")
     val userInput = readln()
-    var hasIngridient = false
 
-    for (i in arrayOfIngridients) {
+    for (i in arrayOfIngredients) {
         if (i == userInput.lowercase()) {
             println("Ингредиент \"$userInput\" в рецепте есть")
-            hasIngridient = true
-            break
+            return
         }
     }
-    if (!hasIngridient) {
-        println("Такого ингредиента в рецепте нет")
-    }
+    println("Такого ингредиента в рецепте нет")
 }
