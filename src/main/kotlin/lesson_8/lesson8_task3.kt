1081a5a24cfc874bad02ab1cfe12c31c727364ab
4meech <hornax@gmail.com>
2024-05-04 12:51:32 +0300

    KS-8-3-2 typo fixed, trim() added, refactored check whether element is inclluded logic

--- src/main/kotlin/lesson_8/lesson8_task3.kt
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -1,14 +1,13 @@
 package org.example.lesson_8
 
 fun main() {
-    val arrayOfIngridients = arrayOf("хлеб", "масло", "колбаса")
+    val arrayOfIngredients = arrayOf("хлеб", "масло", "колбаса")
 
     println("Введите название ингредиента:")
     val userInput = readln()
-    val hasIngridient = arrayOfIngridients.contains(userInput.lowercase())
 
-    if (hasIngridient) {
-        println("Ингредиент \"$userInput\" в рецепте есть")
+    if (userInput.trim().lowercase() in arrayOfIngredients) {
+        println("Ингредиент \"${userInput.trim()}\" в рецепте есть")
     } else {
         println("Такого ингредиента в рецепте нет")
     }
