26c340a888b62e13cf14e384ffed8280ac927cb5
4meech <hornax@gmail.com>
2024-05-07 20:57:03 +0300

    KS-12-5 implemented random weather generation for a specified number of days; calculation of days with precipitation and average temp

---
+++ src/main/kotlin/lesson_12/lesson12_task5.kt
@@ -0,0 +1,60 @@
+package org.example.lesson_12
+
+import kotlin.random.Random
+
+const val DAYS_TOTAL = 30
+
+fun main() {
+
+    val weatherList: MutableList<Wthr> = mutableListOf()
+    val precipitationDayList: MutableList<Wthr> = mutableListOf()
+    val dayTempList = mutableListOf<Int>()
+    val nightTempList = mutableListOf<Int>()
+
+    var dayNumber = 1
+
+    while (weatherList.size < DAYS_TOTAL) {
+
+        val dayTemp = (22..35).random()
+        val nightTemp = (13..20).random()
+        val wasPrecipitation = Random.nextBoolean()
+        val obj = Wthr(dayTemp, nightTemp, wasPrecipitation, "День $dayNumber")
+        dayTempList.add(dayTemp)
+        nightTempList.add(nightTemp)
+        if (wasPrecipitation) precipitationDayList.add(obj)
+        weatherList.add(obj)
+        dayNumber++
+    }
+    println("""
+        ————————————————————————————————————————————
+        Средняя дневная температура за ${dayNumber - 1} дн: ${"%.1f".format(dayTempList.average())}℃
+        Средняя ночная температура за ${dayNumber - 1} дн: ${"%.1f".format(nightTempList.average())}℃
+        Количество дней с осадками: ${precipitationDayList.size}
+    """.trimIndent())
+}
+
+class Wthr(
+    dayTemp: Int,
+    nightTemp: Int,
+    wasPrecipitation: Boolean,
+    name: String,
+) {
+    init {
+        val precipitation = if (wasPrecipitation) "да" else "нет"
+        println("$name. Днём $dayTemp℃, ночью $nightTemp℃, осадки: $precipitation")
+    }
+}
+
+//Продолжи усовершенствовать программу, теперь она должна рассчитывать средние
+//значения температур и считать количество дней с осадками за месяц.
+//
+//Представь, что данные ты получаешь от датчиков. Сымитируй их поведение с помощью random.
+//
+//- в функции main() создай список и добавь в него с помощью цикла 30 объектов класса,
+//передав в конструкторы случайные значения;
+//- сохрани в отдельные списки дневные и ночные температуры, а также список дней с осадками;
+//- вычисли средние значения дневных и ночных температур с помощью специальной
+//extension-функции из стандартной библиотеки;
+//- посчитай количество дней с осадками.
+//
+//Выведи результаты в консоль.
