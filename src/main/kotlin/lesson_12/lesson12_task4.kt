cd3bfbf1a50a0bbe7be47b5d68fb3e8bf089217a
4meech <hornax@gmail.com>
2024-05-08 20:29:19 +0300

    KS-12-4-3 class renamed

--- src/main/kotlin/lesson_12/lesson12_task4.kt
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -5,10 +5,10 @@ const val NIGHT_TEMP_KELVIN = 296
 const val CELSIUS_OFFSET = 273
 
 fun main() {
-    val weatherCentigrade = WeatherKelvin(DAY_TEMP_KELVIN, NIGHT_TEMP_KELVIN, true)
+    val weatherCentigrade = WeatherrKelvin(DAY_TEMP_KELVIN, NIGHT_TEMP_KELVIN, true)
 }
 
-class WeatherKelvin(_dayTemp: Int, _nightTemp: Int, _wasPrecipitation: Boolean) {
+class WeatherrKelvin(_dayTemp: Int, _nightTemp: Int, _wasPrecipitation: Boolean) {
 
     val dayTemp = _dayTemp - CELSIUS_OFFSET
     val nightTemp = _nightTemp - CELSIUS_OFFSET
