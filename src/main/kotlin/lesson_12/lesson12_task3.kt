d410f0862ecd020a364d7b8e1964650e0798255f
4meech <hornax@gmail.com>
2024-05-08 20:17:07 +0300

    KS-12-3-2 calculations moved to class property initialization, magic numbers replaced by constants

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -1,19 +1,23 @@
 package org.example.lesson_12
 
+const val DAY_TEMP_KELVIN = 308
+const val NIGHT_TEMP_KELVIN = 296
+const val CELSIUS_OFFSET = 273
+
 fun main() {
-    val weatherCentigrade = WeatherKelvin(308, 290, true)
+    val weatherCentigrade = WeatherKelvin(DAY_TEMP_KELVIN, NIGHT_TEMP_KELVIN, true)
     weatherCentigrade.printWeather()
 }
 
 class WeatherKelvin(_dayTemp: Int, _nightTemp: Int, _wasPrecipitation: Boolean) {
 
-    val dayTemp = _dayTemp
-    val nightTemp = _nightTemp
+    val dayTemp = _dayTemp - CELSIUS_OFFSET
+    val nightTemp = _nightTemp - CELSIUS_OFFSET
     var wasPrecipitation = _wasPrecipitation
 
     fun printWeather() {
-        println("Дневная температура:   ${dayTemp - 273}℃")
-        println("Ночная температура:    ${nightTemp - 273}℃")
+        println("Дневная температура:   $dayTemp℃")
+        println("Ночная температура:    $nightTemp℃")
         println("Были ли осадки:        $wasPrecipitation")
         println()
     }
