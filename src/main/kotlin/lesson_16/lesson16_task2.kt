fe958d4c4b992ad50e0a8bf5e622d81fd92cb9f5
4meech <hornax@gmail.com>
2024-05-15 20:57:11 +0300

    KS-16-2 private circle

---
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_16
+
+import kotlin.math.pow
+
+const val RADIUS = 10.0
+
+fun main() {
+    val circle = CircleOne(RADIUS)
+
+    println(circle.countLength())
+    println(circle.countArea())
+}
+
+class CircleOne(private val radius: Double) {
+
+    fun countLength(): Double = (2 * PI * radius)
+    fun countArea(): Double = (PI * radius.pow(2))
+
+    companion object {
+        private const val PI = 3.14
+    }
+}
