fa24a415c5fd8a8dd62be104cfffac7fb6c8b3ce
4meech <hornax@gmail.com>
2024-05-18 20:49:49 +0300

    KS-17-3-3 number of files getter added, code shortened

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -4,21 +4,20 @@ fun main() {
     val folder = Folder("X-Files_Scully", 42, true)
     val folder2 = Folder("X-Files_Mulder", 42)
 
-    println(folder.getName)
-    println(folder2.getName)
+    println("${folder.folderName}, файлов: ${folder.numberOfFiles}")
+    println("${folder2.folderName}, файлов: ${folder2.numberOfFiles}")
 }
 
-class Folder (
-    private var folderName: String,
-    private var numberOfFiles: Int,
+class Folder(
+    private var _folderName: String,
+    private var _numberOfFiles: Int,
     private val isSecret: Boolean = false,
 ) {
-    val getName: String
-        get() {
-            return if (isSecret) {
-                "скрытая папка, файлов: 0"
-            } else {
-                "${this.folderName}, файлов: ${this.numberOfFiles}"
-            }
-        }
+    val folderName: String
+        get() = if (isSecret) "Скрытая папка"
+                else "Имя папки: ${this._folderName}"
+
+    val numberOfFiles: Int
+        get() = if (isSecret) 0
+                else this._numberOfFiles
 }
