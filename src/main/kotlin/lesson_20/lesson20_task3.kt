c92a7dcbbbf30e73e68832afad7e3a984f310ee4
4meech <hornax@gmail.com>
2024-05-27 21:20:45 +0300

    KS-20-3 Sesame, open yourself

---
+++ src/main/kotlin/lesson_20/lesson20_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_20
+
+fun main() {
+    val player2 = Player2("Doomguy", true)
+    val player3 = Player2("Doom slayer", false)
+    val checkIfPlayerHasKey: (Player2) -> Unit = {
+        if (it.hasKey) println("${it.playerName} открыл дверь")
+        else println("Дверь заперта, нужен ключ")
+    }
+
+    checkIfPlayerHasKey(player2)
+    checkIfPlayerHasKey.invoke(player3)
+}
+
+private class Player2(
+    val playerName: String,
+    val hasKey: Boolean
+)
