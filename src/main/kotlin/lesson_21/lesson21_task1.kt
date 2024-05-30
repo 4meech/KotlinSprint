774e7b106fdf998e9e2c4cdb43b9305835e557fa
4meech <hornax@gmail.com>
2024-05-30 15:03:46 +0300

    KS-21-1-2 count method implemented instead of loop and counter

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -1,16 +1,11 @@
 package org.example.lesson_21
 
 fun main() {
-    val hello = "HELLO"
-    print(hello.lowercase().vowelCount())
+    val hello = "HELLo"
+    print(hello.vowelCount())
 }
 
 fun String.vowelCount(): Int {
-    var counter = 0
-
-    val vowelsArray = charArrayOf('a','o','u','i','e','y')
-    for (i in this) {
-        if (i in vowelsArray) counter++
-    }
-    return counter
+    val vowelsArray = charArrayOf('a','o','u','i','e','y', 'A', 'O', 'U', 'I', 'E', 'Y')
+    return this.count { it in vowelsArray }
 }
